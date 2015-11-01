79c5ed64ce5b3f08ea1b4056d1c77b9bfa466a1a
Jack Franklin <jack@jackfranklin.net>
2015-11-01 12:37:43 +0000

    Add sequenceInColumn

--- SequenceFinder.elm
+++ SequenceFinder.elm
@@ -16,6 +16,12 @@ getRowOfColours board yIndex =
     |> List.filter (\cell -> cell.y == yIndex)
     |> List.map .colour
 
+getColumnOfColours : Board -> Int -> List Colour
+getColumnOfColours board xIndex =
+  board
+    |> List.filter (\cell -> cell.x == xIndex)
+    |> List.map .colour
+
 updateSequenceMemo : Colour -> Int -> SequenceMemo -> SequenceMemo
 updateSequenceMemo latestColour newCurrentCount oldMemo =
   let
@@ -33,23 +39,35 @@ updateSequenceMemo latestColour newCurrentCount oldMemo =
       longest = newLongest,
       sequenceColour = sequenceColour }
 
+processCellOnBoard : Colour -> SequenceMemo -> SequenceMemo
+processCellOnBoard colour memo =
+  case colour of
+    -- if this cell is empty, current sequence is now 0 and we start again
+    NoColour -> updateSequenceMemo NoColour 0 memo
+    Red -> case memo.last of
+      Red -> updateSequenceMemo Red (memo.current + 1) memo
+      Yellow -> updateSequenceMemo Red 1 memo
+      NoColour -> updateSequenceMemo Red 1 memo
+    Yellow -> case memo.last of
+      Yellow -> updateSequenceMemo Yellow (memo.current + 1) memo
+      Red -> updateSequenceMemo Yellow 1 memo
+      NoColour -> updateSequenceMemo Yellow 1 memo
+
+
+sequenceInColumn: Board -> Int -> { sequence: Int, colour: Colour }
+sequenceInColumn board xIndex =
+  let
+    result = List.foldl processCellOnBoard { sequenceColour = NoColour, last = NoColour, current = 0, longest = 0 } (getColumnOfColours board xIndex)
+  in
+    {
+      sequence = result.longest,
+      colour = result.sequenceColour
+    }
+
 sequenceInRow : Board -> Int -> { sequence: Int, colour: Colour }
 sequenceInRow board yIndex =
   let
-    processRow colour memo =
-      case colour of
-        -- if this cell is empty, current sequence is now 0 and we start again
-        NoColour -> updateSequenceMemo NoColour 0 memo
-        Red -> case memo.last of
-          Red -> updateSequenceMemo Red (memo.current + 1) memo
-          Yellow -> updateSequenceMemo Red 1 memo
-          NoColour -> updateSequenceMemo Red 1 memo
-        Yellow -> case memo.last of
-          Yellow -> updateSequenceMemo Yellow (memo.current + 1) memo
-          Red -> updateSequenceMemo Yellow 1 memo
-          NoColour -> updateSequenceMemo Yellow 1 memo
-
-    result = List.foldl processRow { sequenceColour = NoColour, last = NoColour, current = 0, longest = 0 } (getRowOfColours board yIndex)
+    result = List.foldl processCellOnBoard { sequenceColour = NoColour, last = NoColour, current = 0, longest = 0 } (getRowOfColours board yIndex)
   in
     {
       sequence = result.longest,
--- tests/SequenceFinderTest.elm
+++ tests/SequenceFinderTest.elm
@@ -5,7 +5,7 @@ import ElmTest.Test exposing (test, Test, suite)
 import ElmTest.Assertion exposing (assert, assertEqual)
 
 import ConnectFour exposing (fillCellOnBoard, createBoard, Colour(..))
-import SequenceFinder exposing (sequenceInRow)
+import SequenceFinder exposing (sequenceInRow, sequenceInColumn)
 
 sequenceInRowTestOne =
   let
@@ -42,6 +42,41 @@ sequenceInRowTestThree =
     test "sequence in row test three"
     (assertEqual expected (sequenceInRow finalBoard 0))
 
+sequenceInColumTestOne =
+  let
+    firstBoard = fillCellOnBoard createBoard 0 0 Red
+    secondBoard = fillCellOnBoard firstBoard 0 1 Red
+    finalBoard = fillCellOnBoard secondBoard 0 3 Yellow
+    expected = { sequence = 2, colour = Red }
+  in
+    test "sequence in column test one"
+    (assertEqual expected (sequenceInColumn finalBoard 0))
+
+sequenceInColumnTestTwo =
+  let
+    firstBoard = fillCellOnBoard createBoard 0 0 Red
+    secondBoard = fillCellOnBoard firstBoard 0 1 Red
+    thirdBoard = fillCellOnBoard secondBoard 0 2 Yellow
+    fourthBoard = fillCellOnBoard thirdBoard 0 3 Yellow
+    finalBoard = fillCellOnBoard fourthBoard 0 4 Yellow
+    expected = { sequence = 3, colour = Yellow }
+  in
+    test "sequence in column test two"
+    (assertEqual expected (sequenceInColumn finalBoard 0))
+
+sequenceInColumnTestThree =
+  let
+    firstBoard = fillCellOnBoard createBoard 0 0 Red
+    secondBoard = fillCellOnBoard firstBoard 0 1 Red
+    thirdBoard = fillCellOnBoard secondBoard 0 2 Yellow
+    fourthBoard = fillCellOnBoard thirdBoard 0 3 Red
+    fifthBoard = fillCellOnBoard fourthBoard 0 4 Red
+    finalBoard = fillCellOnBoard fifthBoard  0 5 Red
+    expected = { sequence = 3, colour = Red }
+  in
+    test "sequence in column test three"
+    (assertEqual expected (sequenceInColumn finalBoard 0))
+
 
 tests : Test
 tests =
@@ -50,5 +85,9 @@ tests =
         suite "sequenceInRow"
         [
           sequenceInRowTestOne, sequenceInRowTestTwo, sequenceInRowTestThree
+        ],
+        suite "sequenceInColumn"
+        [
+          sequenceInColumTestOne, sequenceInColumnTestTwo, sequenceInColumnTestThree
         ]
       ]
