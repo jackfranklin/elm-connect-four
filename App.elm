c6065e0f7952e163f9571069d39cead861ee54ea
Jack Franklin <jack@jackfranklin.net>
2016-02-25 10:30:14 -0500

    Update to 0.16

--- App.elm
+++ App.elm
@@ -1,180 +1,232 @@
+module Main (..) where
+
 import Keyboard
 import Mouse
 import Window
 import Text
-
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 import Color exposing (..)
-
-import ConsoleLog exposing (log)
-
 import ConnectFour exposing (..)
+
 
 -- MODEL
 
+
 type alias Model =
-  {
-    board: Board,
-    currentPlayer: Colour,
-    currentMoveCol: Int,
-    winningColour: Colour
+  { board : Board
+  , currentPlayer : Colour
+  , currentMoveCol : Int
+  , winningColour : Colour
   }
+
 
 initialModel : Model
 initialModel =
-  {
-    board = createBoard,
-    currentPlayer = Red,
-    currentMoveCol = 0,
-    winningColour = NoColour
+  { board = createBoard
+  , currentPlayer = Red
+  , currentMoveCol = 0
+  , winningColour = NoColour
   }
+
+
 
 -- UPDATE
 
-type Action =
-  NoOp
+
+type Action
+  = NoOp
   | MovePickerLeft
   | MovePickerRight
   | PlaceCounterBelow
   | RestartGame
 
+
 makeMove : Model -> Model
 makeMove model =
   if columnFull model.currentMoveCol model.board then
-     -- if the column is full NoOp
+    -- if the column is full NoOp
     model
   else
     -- TODO: check if someone has won on newBoard
     let
-      newBoard = placeCounter model.currentMoveCol model.currentPlayer model.board
-      newPlayer = if model.currentPlayer == Red then Yellow else Red
-      hasWon = boardHasWinner newBoard
+      newBoard =
+        placeCounter model.currentMoveCol model.currentPlayer model.board
 
+      newPlayer =
+        if model.currentPlayer == Red then
+          Yellow
+        else
+          Red
+
+      hasWon =
+        boardHasWinner newBoard
     in
-      { model | board <- newBoard,
-                currentPlayer <- newPlayer,
-                currentMoveCol <- 0,
-                winningColour <- if hasWon then model.currentPlayer else NoColour}
+      { model
+        | board = newBoard
+        , currentPlayer = newPlayer
+        , currentMoveCol = 0
+        , winningColour =
+            if hasWon then
+              model.currentPlayer
+            else
+              NoColour
+      }
 
 
 update : Action -> Model -> Model
 update action model =
-  case (log "action" action) of
-    NoOp -> model
+  case action of
+    NoOp ->
+      model
+
     MovePickerLeft ->
       if model.currentMoveCol == 0 then
         model
-       else
-        { model | currentMoveCol <- (model.currentMoveCol - 1) }
+      else
+        { model | currentMoveCol = (model.currentMoveCol - 1) }
+
     MovePickerRight ->
       if model.currentMoveCol == maxXValue then
         model
-       else
-        { model | currentMoveCol <- (model.currentMoveCol + 1) }
+      else
+        { model | currentMoveCol = (model.currentMoveCol + 1) }
+
     PlaceCounterBelow ->
       makeMove model
+
     RestartGame ->
-        initialModel
+      initialModel
+
 
 
 -- VIEW
 
+
 cellColourToGraphic : Colour -> Color
 cellColourToGraphic colour =
   case colour of
-    Red -> red
-    Yellow -> yellow
-    _ -> white
+    Red ->
+      red
+
+    Yellow ->
+      yellow
+
+    _ ->
+      white
+
 
 drawCell : Cell -> Form
 drawCell cell =
   circle 30
-    |> filled (cellColourToGraphic cell.colour)
+    |>
+      filled (cellColourToGraphic cell.colour)
     -- the -3 means the middle column ends up centred on the board
-    |> moveX (toFloat (cell.x - 3) * 80)
-    |> moveY (toFloat (cell.y - 3) * 80)
+    |>
+      moveX (toFloat (cell.x - 3) * 80)
+    |>
+      moveY (toFloat (cell.y - 3) * 80)
 
-drawCounter: Model -> Form
+
+drawCounter : Model -> Form
 drawCounter model =
   circle 30
     |> filled (cellColourToGraphic model.currentPlayer)
     |> moveX (toFloat (model.currentMoveCol - 3) * 80)
-    |> moveY 240.00
+    |> moveY 240.0
+
 
 drawBoard : Board -> Form
 drawBoard board =
   List.map drawCell board |> group
 
-drawBackground : (Float, Float) -> Form
-drawBackground (w, h) =
+
+drawBackground : ( Float, Float ) -> Form
+drawBackground ( w, h ) =
   rect w h |> filled gray
 
-drawWinner : (Float, Float) -> Colour -> Form
-drawWinner (w, h) colour =
+
+drawWinner : ( Float, Float ) -> Colour -> Form
+drawWinner ( w, h ) colour =
   case colour of
-    NoColour -> rect 0 0 |> filled grey
+    NoColour ->
+      rect 0 0 |> filled grey
+
     colour ->
-      [
-        rect (w - 100) (h - 100) |> filled blue,
-        Text.fromString ("Winner! " ++ (toString colour))
+      [ rect (w - 100) (h - 100) |> filled blue
+      , Text.fromString ("Winner! " ++ (toString colour))
           |> Text.color white
           |> Text.height 30
-          |> text,
-        Text.fromString "Press spacebar to restart"
+          |> text
+      , Text.fromString "Press spacebar to restart"
           |> Text.color white
           |> Text.height 15
           |> text
           |> moveY -40
-
-      ] |> group
+      ]
+        |> group
 
 
 drawBoardBackground : Form
 drawBoardBackground =
   rect 560 480 |> filled blue |> moveY -38
 
-view : (Int, Int) -> Model -> Element
-view (w, h) model =
+
+view : ( Int, Int ) -> Model -> Element
+view ( w, h ) model =
   let
-    (w', h') = (toFloat w, toFloat h)
+    ( w', h' ) =
+      ( toFloat w, toFloat h )
   in
-     collage w h [
-       drawBackground (w', h'),
-       drawCounter model,
-       drawBoardBackground,
-       drawBoard model.board,
-       drawWinner (w', h') model.winningColour
-     ]
+    collage
+      w
+      h
+      [ drawBackground ( w', h' )
+      , drawCounter model
+      , drawBoardBackground
+      , drawBoard model.board
+      , drawWinner ( w', h' ) model.winningColour
+      ]
+
 
 
 -- SIGNALS
-
 -- TODO: could merge both below signals into one map
+
+
 columnPicker : Signal Action
 columnPicker =
   Signal.filter (\x -> x /= 0) 0 (Signal.map .x Keyboard.arrows)
-   |> Signal.map (\n -> if n == -1 then MovePickerLeft else MovePickerRight)
+    |> Signal.map
+        (\n ->
+          if n == -1 then
+            MovePickerLeft
+          else
+            MovePickerRight
+        )
 
 
-restart: Signal Action
+restart : Signal Action
 restart =
   Signal.filter ((==) True) False Keyboard.space
-  |> Signal.map (always RestartGame)
+    |> Signal.map (always RestartGame)
+
 
 counterPlacer : Signal Action
 counterPlacer =
   Signal.filter (\x -> x == -1) 0 (Signal.map .y Keyboard.arrows)
     |> Signal.map (always PlaceCounterBelow)
 
+
 input : Signal Action
 input =
-  Signal.mergeMany [columnPicker, counterPlacer, restart]
+  Signal.mergeMany [ columnPicker, counterPlacer, restart ]
 
 
 model : Signal Model
 model =
   Signal.foldp update initialModel input
+
 
 main : Signal Element
 main =
--- ConnectFour.elm
+++ ConnectFour.elm
@@ -1,99 +1,144 @@
-module ConnectFour where
+module ConnectFour (..) where
 
-import Maybe exposing(Maybe(..))
+import Maybe exposing (Maybe(..))
 import Debug
+import List.Extra exposing (group, maximumBy, minimumBy)
 
-import List.Extra exposing(group, maximumBy, minimumBy)
 
-type Colour = Red | Yellow | NoColour
+type Colour
+  = Red
+  | Yellow
+  | NoColour
 
-type alias Board = List Cell
+
+type alias Board =
+  List Cell
+
 
 type alias SequenceMemo =
-  {
-    last: Colour,
-    current: Int,
-    longest: Int,
-    sequenceColour: Colour
+  { last : Colour
+  , current : Int
+  , longest : Int
+  , sequenceColour : Colour
   }
+
 
 type alias SequenceResult =
   { colour : Colour, sequence : Int }
 
-type DiagonalDirection =
-  UpRight | DownRight | DownLeft | UpLeft
 
-type alias Coord = (Int, Int)
+type DiagonalDirection
+  = UpRight
+  | DownRight
+  | DownLeft
+  | UpLeft
+
+
+type alias Coord =
+  ( Int, Int )
+
 
 
 -- board is 6 down, 7 across
-maxXValue = 6
-maxYValue = 5
+
+
+maxXValue =
+  6
+
+
+maxYValue =
+  5
+
 
 type alias Cell =
-  { x: Int, y: Int, colour: Colour }
+  { x : Int, y : Int, colour : Colour }
+
 
 cellIsFilled : Cell -> Bool
 cellIsFilled cell =
   case cell.colour of
-    NoColour -> False
-    _ -> True
+    NoColour ->
+      False
+
+    _ ->
+      True
+
 
 colourCell : Cell -> Colour -> Cell
 colourCell cell newColour =
-  { cell | colour <- newColour }
+  { cell | colour = newColour }
 
-cellMatches: Coord -> Cell -> Bool
-cellMatches (x, y) cell =
+
+cellMatches : Coord -> Cell -> Bool
+cellMatches ( x, y ) cell =
   cell.x == x && cell.y == y
+
 
 findCellOnBoard : Board -> Coord -> Cell
 findCellOnBoard board coords =
   case List.head (List.filter (cellMatches coords) board) of
     --TODO: this is probably not idiomatic Elm...
-    Nothing -> Debug.crash "Cell you searched for doesn't exist"
-    Just cell -> cell
+    Nothing ->
+      Debug.crash "Cell you searched for doesn't exist"
+
+    Just cell ->
+      cell
+
 
 fillCellOnBoard : Coord -> Colour -> Board -> Board
-fillCellOnBoard (x, y) colour board =
+fillCellOnBoard ( x, y ) colour board =
   let
     fillCellIfMatches cell =
-      if cellMatches (x, y) cell then colourCell cell colour else cell
+      if cellMatches ( x, y ) cell then
+        colourCell cell colour
+      else
+        cell
   in
     List.map fillCellIfMatches board
 
+
 createBlankCell : Coord -> Cell
-createBlankCell (x, y) =
+createBlankCell ( x, y ) =
   { x = x, y = y, colour = NoColour }
+
 
 createBoard : List Cell
 createBoard =
   let
-    xValues = [0..maxXValue]
-    yValues = [0..maxYValue]
+    xValues =
+      [0..maxXValue]
+
+    yValues =
+      [0..maxYValue]
   in
-    List.concatMap (\x -> (List.map (\y -> (createBlankCell (x, y))) yValues)) xValues
+    List.concatMap (\x -> (List.map (\y -> (createBlankCell ( x, y ))) yValues)) xValues
+
 
 boardHasWinner : Board -> Bool
 boardHasWinner board =
   .sequence (longestSequenceOnBoard board) == 4
 
-columnFull: Int -> Board -> Bool
+
+columnFull : Int -> Board -> Bool
 columnFull x board =
   let
     colourOfBiggestCell =
       List.filter (\cell -> cell.x == x) board
-        |> maximumBy .y
+        |>
+          maximumBy .y
         -- this Maybe is superfluous, there will always be a cell
-        |> Maybe.withDefault { x = 0, y = 0, colour = NoColour }
-        |> .colour
+        |>
+          Maybe.withDefault { x = 0, y = 0, colour = NoColour }
+        |>
+          .colour
   in
     colourOfBiggestCell /= NoColour
 
-placeCounter: Int -> Colour -> Board -> Board
+
+placeCounter : Int -> Colour -> Board -> Board
 placeCounter x colour board =
   if columnFull x board then
-     board
+    board
   else
     let
       firstEmptyY =
@@ -103,7 +148,7 @@ placeCounter x colour board =
           |> Maybe.withDefault { x = 0, y = 0, colour = NoColour }
           |> .y
     in
-      fillCellOnBoard (x, firstEmptyY) colour board
+      fillCellOnBoard ( x, firstEmptyY ) colour board
 
 
 
@@ -111,64 +156,83 @@ placeCounter x colour board =
 -- and needs to be split out really
 -- but that introduces a circular dependency
 -- the sep of concerns isn't right here
+
+
 getNewCoordsForDirection : DiagonalDirection -> Coord -> Coord
-getNewCoordsForDirection direction (x, y) =
+getNewCoordsForDirection direction ( x, y ) =
   case direction of
     UpRight ->
-      (x + 1, y + 1)
+      ( x + 1, y + 1 )
+
     DownRight ->
-      (x + 1, y - 1)
+      ( x + 1, y - 1 )
+
     DownLeft ->
-      (x - 1, y - 1)
+      ( x - 1, y - 1 )
+
     UpLeft ->
-      (x - 1, y + 1)
+      ( x - 1, y + 1 )
 
 
 getBoundsForDirection : DiagonalDirection -> Coord
 getBoundsForDirection direction =
   case direction of
     UpRight ->
-      (maxXValue, maxYValue)
+      ( maxXValue, maxYValue )
+
     DownRight ->
-      (maxXValue, 0)
+      ( maxXValue, 0 )
+
     DownLeft ->
-      (0, 0)
+      ( 0, 0 )
+
     UpLeft ->
-      (0, maxYValue)
+      ( 0, maxYValue )
 
 
 xOrYAtBounds : DiagonalDirection -> Coord -> Bool
-xOrYAtBounds direction (x, y) =
-  let coords = getBoundsForDirection direction
+xOrYAtBounds direction ( x, y ) =
+  let
+    coords =
+      getBoundsForDirection direction
   in
     x == (fst coords) || y == (snd coords)
 
 
 getNextDiagonalCoords : List Coord -> DiagonalDirection -> Coord -> List Coord
-getNextDiagonalCoords cellList direction (startX, startY) =
+getNextDiagonalCoords cellList direction ( startX, startY ) =
   case List.head (List.reverse cellList) of
-    Nothing -> getNextDiagonalCoords [(startX, startY)] direction (startX, startY)
-    Just (lastX, lastY) ->
-      if | xOrYAtBounds direction (lastX, lastY) -> cellList
-         | otherwise ->
-             let
-               newList = List.append cellList [getNewCoordsForDirection direction (lastX, lastY)]
-             in
-               getNextDiagonalCoords newList direction (startX, startY)
+    Nothing ->
+      getNextDiagonalCoords [ ( startX, startY ) ] direction ( startX, startY )
+
+    Just ( lastX, lastY ) ->
+      if xOrYAtBounds direction ( lastX, lastY ) then
+        cellList
+      else
+        let
+          newList =
+            List.append cellList [ getNewCoordsForDirection direction ( lastX, lastY ) ]
+        in
+          getNextDiagonalCoords newList direction ( startX, startY )
+
 
 buildDiagonalFrom : Coord -> DiagonalDirection -> List Coord
-buildDiagonalFrom (x, y) direction =
-  getNextDiagonalCoords [] direction (x, y)
+buildDiagonalFrom ( x, y ) direction =
+  getNextDiagonalCoords [] direction ( x, y )
+
 
 colourFromCoords : Board -> Coord -> Colour
 colourFromCoords board coords =
   .colour (findCellOnBoard board coords)
 
+
 getDiagonalsFromCell : Board -> Coord -> List (List Colour)
-getDiagonalsFromCell board (x, y) =
-  List.map (\direction ->
-    List.map (colourFromCoords board) (buildDiagonalFrom (x, y) direction)
-  ) [UpRight, DownRight, UpLeft, DownLeft]
+getDiagonalsFromCell board ( x, y ) =
+  List.map
+    (\direction ->
+      List.map (colourFromCoords board) (buildDiagonalFrom ( x, y ) direction)
+    )
+    [ UpRight, DownRight, UpLeft, DownLeft ]
 
 
 getRowOfColours : Board -> Int -> List Colour
@@ -177,37 +241,51 @@ getRowOfColours board yIndex =
     |> List.filter (\cell -> cell.y == yIndex)
     |> List.map .colour
 
+
 getColumnOfColours : Board -> Int -> List Colour
 getColumnOfColours board xIndex =
   board
     |> List.filter (\cell -> cell.x == xIndex)
     |> List.map .colour
 
+
 processListOfColours : List Colour -> SequenceResult
 processListOfColours colours =
-  let longestSeq = group colours
-      |> List.filter (\(colour::_) -> colour /= NoColour)
-      |> maximumBy List.length
-      |> Maybe.withDefault []
+  let
+    longestSeq =
+      group colours
+        |> List.filter
+            (\colours ->
+              (List.head colours |> Maybe.withDefault NoColour) /= NoColour
+            )
+        |> maximumBy List.length
+        |> Maybe.withDefault []
   in
-     case List.head longestSeq of
-       Nothing -> { sequence = 0, colour = NoColour }
-       Just colour -> { sequence = List.length longestSeq, colour = colour }
+    case List.head longestSeq of
+      Nothing ->
+        { sequence = 0, colour = NoColour }
 
-sequenceInColumn: Board -> Int -> SequenceResult
+      Just colour ->
+        { sequence = List.length longestSeq, colour = colour }
+
+
+sequenceInColumn : Board -> Int -> SequenceResult
 sequenceInColumn board xIndex =
   processListOfColours (getColumnOfColours board xIndex)
+
 
 sequenceInRow : Board -> Int -> SequenceResult
 sequenceInRow board yIndex =
   processListOfColours (getRowOfColours board yIndex)
 
-sequenceInDiagonal: Board -> Coord -> SequenceResult
+
+sequenceInDiagonal : Board -> Coord -> SequenceResult
 sequenceInDiagonal board coords =
   -- there are 4 diagonals for a given cell that might have a sequence
   -- so we get them all and sort to find the one with the longest
   -- which is all we really care about
   findLargestSequence <| List.map processListOfColours (getDiagonalsFromCell board coords)
+
 
 findLargestSequence : List SequenceResult -> SequenceResult
 findLargestSequence sequences =
@@ -217,13 +295,17 @@ findLargestSequence sequences =
     |> List.head
     |> Maybe.withDefault { sequence = 0, colour = NoColour }
 
+
 longestSequenceOnBoard : Board -> SequenceResult
 longestSequenceOnBoard board =
   -- TODO: slightly inefficient, if we find a row why bother with columns and diagonals
-  findLargestSequence <| List.concat [
-    List.map (sequenceInRow board) [0..maxYValue],
-    List.map (sequenceInColumn board) [0..maxXValue],
-    List.concatMap (\x ->
-      List.map (\y -> sequenceInDiagonal board (x, y)) [0..maxYValue]
-    ) [0..maxXValue]
-  ]
+  findLargestSequence
+    <| List.concat
+        [ List.map (sequenceInRow board) [0..maxYValue]
+        , List.map (sequenceInColumn board) [0..maxXValue]
+        , List.concatMap
+            (\x ->
+              List.map (\y -> sequenceInDiagonal board ( x, y )) [0..maxYValue]
+            )
+            [0..maxXValue]
+        ]
